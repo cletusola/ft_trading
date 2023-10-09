c2109fb4193740aaebab131a050bbaa6fe555680
cletus ola <136508148+cletusola@users.noreply.github.com>
2023-10-09 03:40:49 -0700

    Update settings.py

--- trade_app/settings.py
+++ trade_app/settings.py
@@ -72,13 +72,19 @@ WSGI_APPLICATION = 'trade_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'myblog',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
